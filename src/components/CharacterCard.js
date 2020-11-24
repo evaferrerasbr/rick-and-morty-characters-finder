3e0a6792f8b7f84c895b08c1a1bdd3123bff687e
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-24 21:31:47 +0100

    Add basic styles to character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,7 +5,11 @@ import icon from '../images/dead-icon.png';
 function CharacterCard(props) {
   const renderIcon = () => {
     return status === 'Dead' ? (
-      <img src={icon} alt="This character is no longer alive" />
+      <img
+        className="CharacterList__list--icon"
+        src={icon}
+        alt="This character is no longer alive"
+      />
     ) : null;
   };
 
@@ -13,8 +17,12 @@ function CharacterCard(props) {
   return (
     <Link to={`/character/${id}`}>
       <article>
-        <img src={image} alt={`${name}, Rick and Mortie character`} />
-        <h3>{name}</h3>
+        <img
+          className="CharacterList__list--img"
+          src={image}
+          alt={`${name}, Rick and Mortie character`}
+        />
+        <h3 className="CharacterList__list--name">{name}</h3>
         <ul>
           <li>Species: {species}</li>
           <li>Status:{status}</li>
