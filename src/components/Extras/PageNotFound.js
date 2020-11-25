7e743cea94308ede2abf9c37a0ba458690f4f16c
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-25 11:34:37 +0100

    Add PageNotFound styles

--- src/components/Extras/PageNotFound.js
+++ src/components/Extras/PageNotFound.js
@@ -1,12 +1,39 @@
 import { Link } from 'react-router-dom';
+import '../../stylesheets/extras/PageNotFound.scss';
+import pagenotfound from '../../images/page-not-found.png';
 
 function PageNotFound() {
   return (
-    <>
-      <p>La página a la que estás intentando navegar no existe.</p>
-      <Link to="/">Volver a la página principal</Link>
-    </>
+    <article className="PageNotFound">
+      <img
+        className="PageNotFound__img"
+        src={pagenotfound}
+        alt="No results for your search"
+      />
+      <p className="PageNotFound__text">
+        Uh, you are trying to navegate to a page I haven't developed
+      </p>
+      <Link className="PageNotFound__link" to="/">
+        Go back home
+      </Link>
+    </article>
   );
 }
 
 export default PageNotFound;
+
+/* <article className="MissingCharacter">
+<div className="MissingCharacter__wrapper">
+  <img
+    className="MissingCharacter__img"
+    src={missingcharacter}
+    alt="No results for your search"
+  />
+  <p className="MissingCharacter__text">
+    Holly shit, the character you want doesn't exist
+  </p>
+</div>
+<Link className="MissingCharacter__link" to="/">
+  Go back to all characters
+</Link>
+</article> */
