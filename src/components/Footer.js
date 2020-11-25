454a05839865b95bea2f54d6afb18cb0a745c497
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-25 08:53:23 +0100

    Add footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
+import '../stylesheets/components/Footer.scss';
+
 function Footer() {
   return (
     <footer className="Footer">
       <p className="Footer__text">
         Developed by{' '}
         <a
-          className="link"
+          className="Footer__link"
           href="https://www.github.com/evaferrerasbr"
           target="blank"
         >
