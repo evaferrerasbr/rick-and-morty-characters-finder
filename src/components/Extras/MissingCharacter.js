83640960f87c9adc7ca757a07718df7da60acde0
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-25 11:24:44 +0100

    Add MissingCharacter styles for mobile

--- src/components/Extras/MissingCharacter.js
+++ src/components/Extras/MissingCharacter.js
@@ -1,11 +1,24 @@
 import { Link } from 'react-router-dom';
+import '../../stylesheets/extras/MissingCharacter.scss';
+import missingcharacter from '../../images/missing-character.png';
 
 function MissingCharacter() {
   return (
-    <>
-      <p>Holly shit, the character you want doesn't exist</p>
-      <Link to="/">Go back to all characters</Link>
-    </>
+    <article className="MissingCharacter">
+      <div className="MissingCharacter__wrapper">
+        <img
+          className="MissingCharacter__img"
+          src={missingcharacter}
+          alt="No results for your search"
+        />
+        <p className="MissingCharacter__text">
+          Holly shit, the character you want doesn't exist
+        </p>
+      </div>
+      <Link className="MissingCharacter__link" to="/">
+        Go back to all characters
+      </Link>
+    </article>
   );
 }
 
